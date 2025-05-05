f6236ac0d7ae6754309d1100fcc8be81107dba86
Anshu Kumar singh <anshukrsingh11@gmail.com>
2025-05-05 23:24:40 +0530

    feat: add Azure Blob Storage integration and middleware
    - Added  hook for file uploads to Azure Blob Storage.
    - Introduced middleware for route handling and authentication checks.
    - Updated  to enable experimental node middleware.
    - Upgraded  to version .
    - Added  dependency for Azure integration.
    - Created  for managing public and auth routes.
    - Added  service for Azure Blob Storage client setup.

---
+++ src/lib/auth/routes.ts
@@ -0,0 +1,9 @@
+export const routes = {
+  publicRoutes: ["/"],
+  authRoutes: [
+    "/auth/sign-in",
+    "/auth/sign-up",
+    "/auth/forgot-password",
+    "/auth/set-password",
+  ],
+}
---
+++ src/middleware.ts
@@ -0,0 +1,59 @@
+import { headers as nextHeaders } from "next/headers"
+import { NextRequest, NextResponse } from "next/server"
+import { auth } from "./lib/auth"
+import { routes } from "./lib/auth/routes"
+
+export async function middleware(request: NextRequest) {
+  const { nextUrl } = request
+  const pathname = nextUrl.pathname
+
+  if (pathname.startsWith("/api/auth")) {
+    return NextResponse.next()
+  }
+
+  const headers = await nextHeaders()
+  const [session, orgs] = await Promise.all([
+    auth.api.getSession({ headers }),
+    auth.api.listOrganizations({ headers }),
+  ])
+
+  const isAuthenticated = !!session?.session
+  const isPublicRoute = routes.publicRoutes.includes(pathname)
+  const isAuthRoute = routes.authRoutes.includes(pathname)
+
+  if (isAuthenticated) {
+    const onboardingStep = session?.user.onboardingStep
+    console.log("onboardingStep", onboardingStep)
+    if (
+      session.user.onboardingStatus === "pending" &&
+      pathname !== `/onboarding/${onboardingStep}`
+    ) {
+      return NextResponse.redirect(new URL(`/onboarding/${onboardingStep}`, nextUrl))
+    }
+
+    if (!orgs.length && pathname !== "/join" && !pathname.startsWith("/onboarding/")) {
+      return NextResponse.redirect(new URL("/join", request.nextUrl))
+    }
+
+    if (
+      (isPublicRoute || isAuthRoute) &&
+      orgs.length &&
+      session.user.onboardingStatus !== "pending"
+    ) {
+      return NextResponse.redirect(new URL(`/${orgs[0].slug}/dashboard`, request.nextUrl))
+    }
+
+    return NextResponse.next()
+  }
+
+  if (isPublicRoute || isAuthRoute) {
+    return NextResponse.next()
+  }
+
+  return NextResponse.redirect(new URL("/auth/sign-in", request.nextUrl))
+}
+
+export const config = {
+  runtime: "nodejs",
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
+}
